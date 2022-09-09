6c1e0940fa353dfc89719d34dfa122cf3210479c
VilosmZX <jravaell@gmail.com>
2022-09-09 17:06:10 +0700

    handle socket on start

--- web/sockets/index.js
+++ web/sockets/index.js
@@ -1,11 +1,22 @@
 const { Server } = require("socket.io");
 const { Client } = require("whatsapp-web.js");
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 const handleSocket = (io = new Server()) => {
     io.on('connection', (socket) => {
         console.log(`Connected ${socket.id}`);
         socket.on('disconnect', () => {
             console.log(`Disconnected ${socket.id}`);
+        })
+
+        socket.on('start', async () => {
+            console.log('Start Triggered!');
+            prisma.chatLog.findMany()
+                .then((chats) => {
+                    socket.emit('load_msg', chats);
+                })
+                .catch(err => console.error(err));
         })
     });
 }

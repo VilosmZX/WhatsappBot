767987b30d003c67a40762c735b08a3364759fc1
VilosmZX <jravaell@gmail.com>
2022-09-10 05:49:30 +0700

    added on clear message handler

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,6 +8,7 @@ socket.on('load_msg', (chats) => {
     const span = document.createElement('span');
     span.innerText = data?.value;
     div.appendChild(span);
+    window.scrollTo(0, document.body.scrollHeight);
   });
 })
 
@@ -16,4 +17,14 @@ socket.on('new_msg', (data) => {
   const span = document.createElement('span');
   span.innerText = `(${data?.chat}) From ${data.author?.number} AKA ${data.author?.name}: ${data.msg}`;
   div.appendChild(span);
+  window.scrollTo(0, document.body.scrollHeight);
 });
+
+socket.on('clear_msg_done', () => {
+  const div = document.querySelector('div');
+  div.innerHTML = '';
+});
+
+function clearMsg() {
+  socket.emit('clear_msg');
+}

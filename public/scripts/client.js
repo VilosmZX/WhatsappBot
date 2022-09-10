aebcbd1658311314cb15e127226653713e5a7cba
VilosmZX <jravaell@gmail.com>
2022-09-10 14:09:20 +0700

    Added link on phone number

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,28 +3,32 @@ const socket = io();
 socket.emit('start');
 
 socket.on('load_msg', (chats) => {
-  const div = document.querySelector('div');
+  const div = document.querySelector('.chatlog__container');
   chats.forEach((data) => {
     const span = document.createElement('span');
-    span.innerText = data?.value;
+    span.innerHTML = `(${data?.group}) From <a href="https://wa.me/${data?.number}">${data?.number}</a> AKA ${data?.name}: ${data?.msg}`;
     div.appendChild(span);
-    window.scrollTo(0, document.body.scrollHeight);
+    div.scrollTo(0, div.scrollHeight);
   });
 })
 
 socket.on('new_msg', (data) => {
-  const div = document.querySelector('div');
+  const div = document.querySelector('.chatlog__container');
   const span = document.createElement('span');
-  span.innerText = `(${data?.chat}) From ${data.author?.number} AKA ${data.author?.name}: ${data.msg}`;
+  span.innerHTML = `(${data?.chat}) From <a href="https://wa.me/${data.author?.number}">${data.author?.number}</a> AKA ${data.author?.pushname}: ${data.msg}`;
   div.appendChild(span);
-  window.scrollTo(0, document.body.scrollHeight);
+  div.scrollTo(0, div.scrollHeight);
 });
 
 socket.on('clear_msg_done', () => {
-  const div = document.querySelector('div');
+  const div = document.querySelector('.chatlog__container');
   div.innerHTML = '';
 });
 
 function clearMsg() {
   socket.emit('clear_msg');
+}
+
+function navigate(location) {
+  window.location.replace(location);
 }

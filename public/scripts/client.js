d79159fe7b684764e06b0fe1ee2f0c31a88bbafa
VilosmZX <jravaell@gmail.com>
2022-09-10 14:57:34 +0700

    Added functionality to copy message by clicking

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,6 +7,8 @@ socket.on('load_msg', (chats) => {
   chats.forEach((data) => {
     const span = document.createElement('span');
     span.innerHTML = `(${data?.group}) From <a href="https://wa.me/${data?.number}">${data?.number}</a> AKA ${data?.name}: ${data?.msg}`;
+    span.classList.add('msg');
+    span.onclick = () => copyMsg(data.msg);
     div.appendChild(span);
     div.scrollTo(0, div.scrollHeight);
   });
@@ -16,6 +18,8 @@ socket.on('new_msg', (data) => {
   const div = document.querySelector('.chatlog__container');
   const span = document.createElement('span');
   span.innerHTML = `(${data?.chat}) From <a href="https://wa.me/${data.author?.number}">${data.author?.number}</a> AKA ${data.author?.pushname}: ${data.msg}`;
+  span.classList.add('msg');
+  span.onclick = () => copyMsg(data.msg);
   div.appendChild(span);
   div.scrollTo(0, div.scrollHeight);
 });
@@ -31,4 +35,17 @@ function clearMsg() {
 
 function navigate(location) {
   window.location.replace(location);
+}
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function copyMsg(msg) {
+  navigator.clipboard.writeText(msg);
+  const div = document.querySelector('.info__container');
+  div.firstElementChild.firstElementChild.innerText = `Copied "${msg}"`
+  div.classList.remove('hideInfo');
+  await delay(2000);
+  div.classList.add('hideInfo');
 }
